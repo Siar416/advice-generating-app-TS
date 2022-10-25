c94af126cd3e4118f316798c6b4a2cec0a0739e2
Siar Wahidi <siar.wahidi@gmail.com>
2022-10-25 10:45:13 -0400

    Added type string to advice state. Added useEffect hook to fetch data

--- src/components/Advice.tsx
+++ src/components/Advice.tsx
@@ -4,6 +4,17 @@ import dividerImg from "../assets/pattern-divider-desktop.svg";
 import dice from "../assets/icon-dice.svg";
 
 const Advice = () => {
+    const [advice, setAdvice] = useState<string>("");
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await axios.get("https://api.adviceslip.com/advice");
+            setAdvice(response.data.slip)
+        }
+
+        fetchData();
+    }, []);
+
     return (
         <div className="container">
             <h3 className="advice_heading">ADVICE #</h3>
