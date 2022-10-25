b3302328fd219cd907cabed199a4577b51434972
Siar Wahidi <siar.wahidi@gmail.com>
2022-10-25 11:29:05 -0400

    added types to state id as number and advice as string

--- src/components/Advice.tsx
+++ src/components/Advice.tsx
@@ -4,21 +4,41 @@ import dividerImg from "../assets/pattern-divider-desktop.svg";
 import dice from "../assets/icon-dice.svg";
 
 const Advice = () => {
-    const [advice, setAdvice] = useState<string>("");
+
+
+
+
+    const [advice, setAdvice] = useState<{id: number | null; advice: string}>({
+        id: null,
+        advice: ""
+    });
 
     useEffect(() => {
         const fetchData = async () => {
             const response = await axios.get("https://api.adviceslip.com/advice");
+            console.log(response.data.slip)
             setAdvice(response.data.slip)
         }
 
         fetchData();
     }, []);
 
+    // const handleClick = () => {
+    //     const fetchData = async () => {
+    //         const response = await axios.get("https://api.adviceslip.com/advice");
+    //         setAdvice(response.data.slip)
+    //     }
+    //     fetchData();
+    // }
+
+    if(!advice) {
+        return <h1>Loading advice...</h1>
+    }
+
     return (
         <div className="container">
-            <h3 className="advice_heading">ADVICE #</h3>
-            <p className="advice"></p>
+            <h3 className="advice_heading">ADVICE # {advice.id}</h3>
+            <p className="advice">"{advice.advice}"</p>
 
             <div className="bottom">
                 <div className="divider">
